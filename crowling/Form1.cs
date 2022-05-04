b6bfcf35f8992ec785649592adee3c84d1e267c5
HungKungE <jonsun59@gmail.com>
2022-05-05 00:00:53 +0900

    주석 추가

--- crowling/Form1.cs
+++ crowling/Form1.cs
@@ -15,10 +15,12 @@ namespace crowling
 {
     public partial class Form1 : Form
     {
+        // 도구 -> NuGet 패키지 관리자 -> 솔루션용 NuGet 패키지 관리 -> 찾아보기에서 HtmlAglityPack 검색 후 프로젝트에 설치
         public Form1()
         {
             InitializeComponent();
         }
+
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -27,10 +29,12 @@ namespace crowling
             string url3 = "&endTime=";
             string url4 = "&timeframe=week";
             string url_total = url1 + textBox3.Text +url2 + textBox1.Text + url3 + textBox2.Text + url4;
+            // textBox3 : 종목코드 , textBox1 : 시작일자, textBox2 : 종료일자
+            //url_total 예시 : https://api.finance.naver.com/siseJson.naver?symbol=005930&requestType=1&startTime=20210801&endTime=20210930&timeframe=week
             WebClient wc = new WebClient();
             wc.Encoding = Encoding.UTF8;
-            String html = wc.DownloadString(url_total);
-            // https://api.finance.naver.com/siseJson.naver?symbol=005930&requestType=1&startTime=20210801&endTime=20210930&timeframe=week
+            String html = wc.DownloadString(url_total); 
+            
             HtmlAgilityPack.HtmlDocument htmlDoc = new HtmlAgilityPack.HtmlDocument();
             htmlDoc.LoadHtml(html);
             var div = htmlDoc.Text;
@@ -50,7 +54,8 @@ namespace crowling
             StreamWriter writer = File.CreateText(name);
             writer.WriteLine(div);
             writer.Close();
-            
+            // 시작, 종료, 종목코드를 입력후 버튼 클릭 시 프로젝트의 bin - debug 에 stock.txt가 생기고 정보가 저장됨
+            // 처음 실행 시 에러 생길 수 있음, 한번더 실행하면 정상 실행됨
         }
     }
 }
